10292cf184cd3c7ae9c0f2970cddb5e5438a6af8
root <root@ip-172-31-39-79.ap-northeast-2.compute.internal>
2023-07-28 00:03:55 +0000

    update nodejs server settins

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'what-you-do-today-database.cakpilgd5tuz.ap-northeast-2.rds.amazonaws.com',
+    user: 'master',
     port: '3306',
-    password: '',
-    database: ''
+    password: 'retim0330',
+    database: 'what_you_do_today_database'
 });
 
 module.exports = {
